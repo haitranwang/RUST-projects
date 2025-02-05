c3d3ebcb19cf4e10dcb863bde14c3732dc926899
haitranwang <haitranwang@gmail.com>
2025-02-05 18:25:11 +0700

    Practice TDD

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,12 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
@@ -13,33 +14,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = config.run() {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].to_string();
-        let filename = args[2].to_string();
-
-        Ok(Config { query, filename })
-    }
-
-    fn run(&self) -> Result<(), Box<dyn Error>> {
-        let contents =
-        fs::read_to_string(self.filename.to_string())?;
-        println!("Content: {}", contents);
-
-        Ok(())
     }
 }
